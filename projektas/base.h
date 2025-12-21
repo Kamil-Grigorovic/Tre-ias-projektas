90a152923bc9a933315f29254b23ee6790b515a7
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-12-22 00:56:12 +0200

    Refactor Zmogus class constructor and destructor
    Updated constructor to allow default parameters and modified destructor to clear strings.

--- projektas/base.h
+++ projektas/base.h
@@ -10,31 +10,26 @@ using namespace std;
  * Turi grynai virtualią funkciją info(), todėl negali būti sukurta tiesiogiai.
  */
 class Zmogus {
-private:
+protected:
     string vard; ///< Studento vardas
     string pav; ///< Studento pavardė
-
 public:
-
-    /**
-     * @brief Numatytaisiais parametrais inicializuojantis konstruktorius.
-     *
-     * Sukuria objektą su tuščiu vardu ir pavarde.
-     */
-    Zmogus() : vard(""), pav("") {}
     /**
      * @brief Konstruktorius su vardu ir pavarde.
      * @param v Vardas
      * @param p Pavardė
      */
-    Zmogus(const string& v, const string& p) : vard(v), pav(p) {}
+    Zmogus(const string& v = "", const string& p = "") : vard(v), pav(p) {}
 
     /**
      * @brief Virtualus destruktorius.
      *
      * Leidžia saugiai naikinti objektus per bazinės klasės rodykles.
      */
-    virtual ~Zmogus() = default;
+    virtual ~Zmogus() {
+        vard.clear();
+        pav.clear();
+    }
 
     /**
      * @brief Grąžina žmogaus vardą.
@@ -54,7 +49,7 @@ public:
     void setVard(const string& v) { vard = v; }
     /**
      * @brief Nustato žmogaus pavardę.
-     * @param p Nauja pavardė
+     * @param v Nauja pavardė
      */
     void setPav(const string& p) { pav = p; }
 
@@ -65,4 +60,3 @@ public:
      */
     virtual void info() const = 0;
 };
-
